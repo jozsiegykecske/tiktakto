13ae8f2337e4de72f82b99a0d5d4d4a4e5318032
jozsiegykecske <jozsiegykecske@gmail.com>
2021-11-18 13:45:18 +0100

    Első verzió tökéletesítése!

--- tiktakto/Program.cs
+++ tiktakto/Program.cs
@@ -173,8 +173,10 @@ namespace tiktakto
     }
     static void Kirajzol()
     {
+      Console.WriteLine("\t   1 2 3");
       for (int i = 0; i < 3; i++)
       {
+        Console.Write($"\t {i+1} ");
         for (int j = 0; j < 3; j++)
         {
           SzinesbenKiir(jatekter[i,j]);
